e62a4ce3a1f6bd4b982bb55215ff5e8cd72e2bf5
Sean Phillips <36488351+sbphillips3@users.noreply.github.com>
2019-06-18 19:59:22 -0500

    Formatting numbers
    Here I format a number to a standard format using :n, then format it to hexadecimal using :x, and then convert it back with a Parse method.

---
+++ Formatting/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Sean Phillips
+ * 06/18/2019
+ */
+using System;
+using static System.Console;
+
+namespace Formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 7654321;
+            WriteLine($"This is the number as an formatted int:       {num:n}");// Formated number with the :n --with two decimal places
+            WriteLine($"This is the number formated as a hexadecimal: {num:x}");// Same number formated as hexadecimal
+
+            string hexiValue = num.ToString("X");
+
+            int numConverted = int.Parse(hexiValue, System.Globalization.NumberStyles.HexNumber);
+
+            WriteLine($"And the number converted back using Parse:    {numConverted:n}"); // Same number converted back and formated with :n
+
+            ReadLine();
+        }
+    }
+}
